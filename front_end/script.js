c8f24f90768344944ea0d5f753c9222506295657
kendell joseph <kendell@jct.io>
2018-02-05 18:03:02 +0000

    We have added event listeners to make the buttons display a message in the terminal telling you if you are checking out or in

--- front_end/script.js
+++ front_end/script.js
@@ -1,5 +1,32 @@
 // This is a list of "global variables"
 /* global moment, $ */
+/*
+var checkIn = document.getElementById("check-in");
+var checkOut = document.getElementById("check-out");
+var checkingOut = addEventListener("click", checkOut);
+var checkingIn = addEventListener("click", checkIn);
+if (checkingIn){
+    alert("check in clicked!");
+}
+if (checkingOut){
+    alert("check out clicked!");
+}
+*/
+
+document.querySelector("#check-in").addEventListener("click", () => {
+  console.log(`You're checked in!`);
+});
+document.querySelector("#check-out").addEventListener("click", () => {
+  console.log(`You're checked out!`);
+});
+/*
+$(function(){
+    $("#checkIn").click(function(){
+        alert('clicked!');
+    });
+});
+*/
+
 
 function updateClock() {
   var now = moment(),
@@ -15,3 +42,17 @@ function timedUpdate () {
   setTimeout(timedUpdate, 1000);
 }
 timedUpdate();
+
+// Function to change the content of t2
+function modifyText() {
+  var t2 = document.getElementById("t2");
+  if (t2.firstChild.nodeValue == "three") {
+    t2.firstChild.nodeValue = "two";
+  } else {
+    t2.firstChild.nodeValue = "three";
+  }
+}
+
+// add event listener to table
+var el = document.getElementById("outside");
+el.addEventListener("click", modifyText, false);
