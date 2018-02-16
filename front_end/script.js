544c387842d0eb006d819cafd1035b62e8156dd7
kendell joseph <kendell@jct.io>
2018-02-16 18:10:38 +0000

    adding the clock element

--- front_end/script.js
+++ front_end/script.js
@@ -1,17 +1,5 @@
 // This is a list of "global variables"
 /* global moment, $ */
-/*
-var checkIn = document.getElementById("check-in");
-var checkOut = document.getElementById("check-out");
-var checkingOut = addEventListener("click", checkOut);
-var checkingIn = addEventListener("click", checkIn);
-if (checkingIn){
-    alert("check in clicked!");
-}
-if (checkingOut){
-    alert("check out clicked!");
-}
-*/
 
 document.querySelector("#check-in").addEventListener("click", () => {
   console.log(`You're checked in!`);
@@ -19,40 +7,26 @@ document.querySelector("#check-in").addEventListener("click", () => {
 document.querySelector("#check-out").addEventListener("click", () => {
   console.log(`You're checked out!`);
 });
-/*
-$(function(){
-    $("#checkIn").click(function(){
-        alert('clicked!');
-    });
-});
-*/
 
-
+// This function updates the clock by changing the css
 function updateClock() {
+  // This is something from momentjs, i think it gets the time?
   var now = moment(),
       second = now.seconds() * 6,
       minute = now.minutes() * 6 + second / 60,
       hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+  
+  // These are the elements that are changed using jquery
   $('#hour').css("transform", "rotate(" + hour + "deg)");
   $('#minute').css("transform", "rotate(" + minute + "deg)");
   $('#second').css("transform", "rotate(" + second + "deg)");
 }
-function timedUpdate () {
+
+// This is a function that is run every second
+function timedUpdate() {
   updateClock();
   setTimeout(timedUpdate, 1000);
 }
+
+// This starts the function that runs every second
 timedUpdate();
-
-// Function to change the content of t2
-function modifyText() {
-  var t2 = document.getElementById("t2");
-  if (t2.firstChild.nodeValue == "three") {
-    t2.firstChild.nodeValue = "two";
-  } else {
-    t2.firstChild.nodeValue = "three";
-  }
-}
-
-// add event listener to table
-var el = document.getElementById("outside");
-el.addEventListener("click", modifyText, false);
