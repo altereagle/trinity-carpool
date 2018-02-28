3be888c2d2ae55cfb04b19f9173573a8f72f797b
kendell joseph <kendell@jct.io>
2018-02-28 18:14:37 +0000

    Updates to the index.html file

--- front_end/script.js
+++ front_end/script.js
@@ -8,25 +8,11 @@ document.querySelector("#check-out").addEventListener("click", () => {
   console.log(`You're checked out!`);
 });
 
-// This function updates the clock by changing the css
-function updateClock() {
-  // This is something from momentjs, i think it gets the time?
-  var now = moment(),
-      second = now.seconds() * 6,
-      minute = now.minutes() * 6 + second / 60,
-      hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
-  
-  // These are the elements that are changed using jquery
-  $('#hour').css("transform", "rotate(" + hour + "deg)");
-  $('#minute').css("transform", "rotate(" + minute + "deg)");
-  $('#second').css("transform", "rotate(" + second + "deg)");
+// this adds  a clock 
+//$('#clock').fitText(1.3);
+
+function update() {
+  $('#clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
 }
 
-// This is a function that is run every second
-function timedUpdate() {
-  updateClock();
-  setTimeout(timedUpdate, 1000);
-}
-
-// This starts the function that runs every second
-timedUpdate();
+setInterval(update, 1000); 
